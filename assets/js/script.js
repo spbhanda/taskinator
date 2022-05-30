b95cc78d9582e9c8a9dc86b0b0975a69db097413
spbhanda <spbhanda@gmail.com>
2022-05-30 18:51:22 -0500

    complete form-submit task

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,12 @@ var taskFormHandler = function (event) {
    var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
    // package up data as an object
+   // check if input values are empty strings
+   if (!taskNameInput || !taskTypeInput) {
+      alert("You need to fill out the task form!");
+      return false;
+   }
+   formEl.reset();
    var taskDataObj = {
       name: taskNameInput,
       type: taskTypeInput,
